111f6f14d2864b3a4c08f6bbab1412bbb7594a2b
Saliza Rana Magar <salizarana@gmail.com>
2020-01-11 16:54:58 +0545

    Reverse order

---
+++ examples/QN006.c
@@ -0,0 +1,27 @@
+/**
+ * Write a program that takes four digit number from a user and 
+   displays the number in reverse order.
+ */
+
+#include<stdio.h>
+
+int main() {
+    int num, temp;
+
+    printf("Enter any four digit number : ");
+    scanf("%d", &num);
+
+    temp = num%10;
+    printf("Reverse is %d", temp);
+    num = num/10;
+    temp = num%10;
+    printf("%d", temp);
+    num = num/10;
+    temp = num%10;
+    printf("%d", temp);
+    num = num/10;
+    temp = num%10;
+    printf("%d/n", temp);
+
+    return 0;
+}
