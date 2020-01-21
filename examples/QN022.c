365dc26c97cca00feb451c20fa4b4e3442927fa9
Saliza Rana Magar <salizarana@gmail.com>
2020-01-21 17:18:12 +0545

    Update logic to check leap year.

--- examples/QN022.c
+++ examples/QN022.c
@@ -11,20 +11,8 @@ int main()
   printf("Enter any year: ");
   scanf("%d", &year);
 
-  if (year % 4 == 0)
-  {
-    if (year % 100 == 0)
-    {
-      if (year % 400 == 0)
-      {
-        printf("%d is a leap year.\n", year);
-      }
-      else
-        printf("%d is not a leap year.\n", year);
-    }
-    else
-      printf("%d is not a leap year.\n", year);
-  }
+  if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+    printf("%d is a leap year.\n", year);
   else
     printf("%d is not a leap year.\n", year);
 
