a80e9bc717641c56d6518ebc4859d7a564a3a601
Saliza Rana Magar <salizarana@gmail.com>
2020-06-12 21:15:19 +0545

    Rename variable.

--- chapters/array/QN006.c
+++ chapters/array/QN006.c
@@ -6,15 +6,15 @@
 
 int main()
 {
-  int r, c, i, j, a[10][10], transpose[10][10];
+  int row, column, i, j, a[10][10], transpose[10][10];
 
   printf("Enter rows and columns:\n");
-  scanf("%d %d", &r, &c);
+  scanf("%d %d", &row, &column);
 
   printf("Enter matrix elements:\n");
-  for (i = 0; i < r; i++)
+  for (i = 0; i < row; i++)
   {
-    for (j = 0; j < c; j++)
+    for (j = 0; j < column; j++)
     {
       printf("Enter element a%d%d: ", i + 1, j + 1);
       scanf("%d", &a[i][j]);
@@ -22,21 +22,21 @@ int main()
   }
 
   // Finding transpose of matrix.
-  for (i = 0; i < r; i++)
+  for (i = 0; i < row; i++)
   {
-    for (j = 0; j < c; j++)
+    for (j = 0; j < column; j++)
     {
       transpose[j][i] = a[i][j];
     }
   }
 
   printf("\nTranspose of the matrix:\n");
-  for (i = 0; i < c; i++)
+  for (i = 0; i < column; i++)
   {
-    for (j = 0; j < r; j++)
+    for (j = 0; j < row; j++)
     {
       printf("%d  ", transpose[i][j]);
-      if (j == r - 1)
+      if (j == row - 1)
         printf("\n");
     }
   }
