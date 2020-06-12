95ff6fdd2bf0bfb4ef7a58aef179763929559fc4
Saliza Rana Magar <salizarana@gmail.com>
2020-06-12 09:01:53 +0545

    Fix issue of not storing multiple student.

--- chapters/file-handling/QN007.c
+++ chapters/file-handling/QN007.c
@@ -5,26 +5,51 @@
 
 #include <stdio.h>
 
+struct Student
+{
+  char name[20];
+  char grade[1];
+  int roll;
+  int age;
+};
+
 int main()
 {
-  FILE *f;
-  char name[10], grade;
-  int roll, age, n, i;
+  FILE *fp;
+  int i, n;
 
-  printf("Enter the number of students:\n");
+  printf("Enter the total number of student:\n");
   scanf("%d", &n);
-  printf("Enter name, roll, age and grade of students:\n");
-  f = fopen("Student", "w");
+
+  struct Student s[n];
+
+  printf("Enter the details of student:\n");
+  fp = fopen("student.bin", "w");
   for (i = 0; i < n; i++)
   {
-    fscanf(stdin, "%s %d %d %c", name, &roll, &age, &grade);
+    printf("Name:\n");
+    scanf("%s", s[i].name);
+    printf("Grade:\n");
+    scanf("%s", s[i].grade);
+    printf("Roll:\n");
+    scanf("%d", &s[i].roll);
+    printf("Age:\n");
+    scanf("%d", &s[i].age);
+    fprintf(fp, "%s %s %d %d", s[i].name, s[i].grade, s[i].roll, s[i].age);
   }
-  fclose(f);
+  fclose(fp);
 
-  f = fopen("Student", "r");
+  fp = fopen("student.bin", "r");
+  printf("\nDisplaying information of all students:\n");
   for (i = 0; i < n; i++)
-    fprintf(stdout, "Name:%s\nRoll:%d\nAge:%d\nGrade:%c\n", name, roll, age, grade);
-  fclose(f);
+  {
+    fscanf(fp, "%s %c %d %d", s[i].name, s[i].grade, &s[i].roll, &s[i].age);
+    printf("\nName: %s", s[i].name);
+    printf("\nGrade: %s", s[i].grade);
+    printf("\nRoll: %d", s[i].roll);
+    printf("\nAge: %d\n", s[i].age);
+  }
+  fclose(fp);
 
   return 0;
 }
