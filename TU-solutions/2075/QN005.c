34068a411f7c2a67a88ea39bcee41c9b4b2cc796
Saliza Rana Magar <salizarana@gmail.com>
2020-06-16 21:10:14 +0545

    Remove unused.

--- TU-solutions/2075/QN005.c
+++ TU-solutions/2075/QN005.c
@@ -8,14 +8,11 @@ int main()
 {
   int i, sum = 0;
 
-  if (10 > 0)
+  for (i = 1; i <= 10; i++)
   {
-    for (i = 1; i <= 10; i++)
-    {
-      sum = sum + 2 * i - 1;
-    }
-    printf("\nSum of first 10 odd numbers: %d\n", sum);
+    sum = sum + 2 * i - 1;
   }
+  printf("Sum of first 10 odd numbers: %d\n", sum);
 
   return 0;
 }
